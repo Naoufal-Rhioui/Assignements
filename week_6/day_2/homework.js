5225e5c8cd078953a7055ba86a56d2fd187e6ca1
Naoufal-Rhioui <57770750+Naoufal-Rhioui@users.noreply.github.com>
2021-06-30 00:52:43 -0400

    Update homework.js

--- week_6/day_2/homework.js
+++ week_6/day_2/homework.js
@@ -1,29 +1,89 @@
-//==================Exercise #1 ==========//
+//==========Exercise #1 ===========//
+/*
+Write a function that parses through the below object and displays all of their
+favorite food dishes as shown:
+*/
 
-let dog_string = "Hello Max, my name is Dog, and I have purple eyes!"
-let dog_names = ["Max","HAS","PuRple","dog"]
+let person3 = {
+    pizza:["Deep Dish","South Side Thin Crust"],
+    tacos:"Anything not from Taco bell",
+    burgers:"Portillos Burgers",
+    ice_cream:["Chocolate","Vanilla","Oreo"],
+    shakes:[{
+        oberwise:"Chocolate",
+        dunkin:"Vanilla",
+        culvers:"All of them",
+        mcDonalds:"Sham-rock-shake",
+        cupids_candies:"Chocolate Malt"
+    }]
+}
 
-function findWords(){
-    for(let i=0; i<dog_names.length; i++){
-        if (dog_string.includes(dog_names[i])){
-            console.log("Matched " + dog_names[i])
-            break
+function favoriteFoods(object1) {
+    for (let i = 0; i < Object.entries(object1).length; i++) {
+        console.log(Object.keys(object1)[i], Object.values(object1)[i]);
+    }
+}
+
+favoriteFoods(person3)
+
+//=======Exercise #2=========//
+/*
+Write an object prototype for a Person that has a name and age, has a
+printInfo method, and also has a method that 
+increments the persons age by 1 each time the method is called.
+Create two people using the 'new' keyword, and print 
+both of their infos and increment one persons
+age by 3 years. Use an arrow function for both methods
+*/
+
+// Create our Person Prototype
+function Person(name,age){
+    this.name = name;
+    this.age = age;
+    // Use an arrow to create the printInfo method
+    this.printInfo = () => {
+        console.log(`This is ${this.name}, who is ${this.age} years old.`);
+    }
+    // Create another arrow function for the addAge method that takes a single parameter
+    // Adding to the age
+    this.addAge = () => {
+        this.age += 1
+        console.log(`${this.name} is now ${this.age} years old.`)
+    }
+}
+
+let someone1 = new Person('Shakira', 44)
+let someone2 = new Person('DjKhaled', 45)
+console.log(someone1.printInfo())
+console.log(someone1.addAge())
+console.log(someone1.addAge())
+console.log(someone1.addAge())
+console.log(someone2.printInfo())
+
+
+
+
+// =============Exercise #3 ============//
+/*
+    Create a Promised based function that will check a string to determine if it's length is greater than 10.
+    If the length is greater than ten console log "Big word". 
+    If the length of the string is less than 10 console log "Small Number"
+*/
+
+const aString = (strings) => {
+    return new Promise( (resolve,reject) => {
+        if(strings.length >= 10){
+            resolve(strings)
         } else {
-            return 'No Matches'
+            reject(strings)
         }
-    }
-}
-console.log(findWords(dog_string,dog_names))
-
-//============Exercise #2 ============//
-
-let arr = ["Max","Baseball","Reboot","Goku","Trucks","Rodger"]
-
-function replaceEvens(){
-    for (let i = 0; i < arr.length; i+=2) {
-        arr.splice(i, 1, 'even index')
-    }
-    return arr
+    })
 }
 
-console.log(replaceEvens(arr))
+aString('Short.')
+.then( (strings) => {
+    console.log(`Big word: ${strings}`)
+})
+.catch( (strings) => {
+    console.log(`Small word: ${strings}`)
+})
